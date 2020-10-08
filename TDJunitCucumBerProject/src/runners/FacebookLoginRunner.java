a6e5fda0388392762d0e2b0c672ec27f64f0b274
eluemeln <47062807+eluemeln@users.noreply.github.com>
2020-10-07 21:40:30 -0600

    Update FacebookLoginRunner.java

--- TDJunitCucumBerProject/src/runners/FacebookLoginRunner.java
+++ TDJunitCucumBerProject/src/runners/FacebookLoginRunner.java
@@ -1,17 +1,1 @@
-package runners;
 
-import org.junit.runner.RunWith;
-
-import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
-
-@RunWith(Cucumber.class)
-@CucumberOptions( plugin = { "pretty", "html:target/cucumber-reports","json:target/cucumber-reports/Cucumber.json",
-		"junit:target/cucumber-reports/Cucumber.xml"},features ="resources/features/fcbLogin.feature",
-glue = {"stepdefinitions"},monochrome = true)
-
-public class FacebookLoginRunner  {
-	
-	
-
-}
